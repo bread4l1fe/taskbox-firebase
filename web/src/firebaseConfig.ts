f7cb39956b1cd96d3311ac370d5967e7e7b46a11
bread4l1fe <matutech0301@gmail.com>
2025-09-29 15:11:05 -0400

    Update and rename firebaseConfig.sample.ts to firebaseConfig.ts

---
+++ web/src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDaQbllxi5SiWD-brBP8jyfegoWfxC7lz8",
+  authDomain: "taskbox-matt.firebaseapp.com",
+  projectId: "taskbox-matt",
+  storageBucket: "taskbox-matt.firebasestorage.app",
+  messagingSenderId: "232444501415",
+  appId: "1:232444501415:web:ad1fd5c291033a82d6ea82"
+};
